2449c7cf0996aa1850ed2d0fc09b39251ea1f0b5
Taoufik <tbouchikhi1@hotmail.com>
2021-03-02 01:37:36 +0000

    NARC remove pdb

--- src/textractor.py
+++ src/textractor.py
@@ -133,8 +133,6 @@ class Textractor:
 
         ips = None
         try:
-            import pdb
-            pdb.set_trace()
             ips = self.validateInput(sys.argv)
         except Exception as e:
             self.printFormatException(e)
